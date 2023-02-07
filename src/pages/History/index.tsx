762beaec66ce1b703bb9c32c2a6dcd52b75a03bf
leticea <nascimento.leticeah@gmail.com>
2023-02-07 16:19:01 -0300

    recebendo os dados do histórico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -3,8 +3,8 @@ import { CyclesContext } from "../../contexts/CyclesContext";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
-  const { cycles } = useContext(CyclesContext)
-  
+  const { cycles } = useContext(CyclesContext);
+
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
@@ -20,40 +20,21 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td><Status statusColor="green">Concluído</Status></td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td><Status statusColor="green">Concluído</Status></td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td><Status statusColor="green">Concluído</Status></td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td><Status statusColor="yellow">Em andamento</Status></td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td><Status statusColor="red">Interrompido</Status></td>
-            </tr>
+            {cycles.map((cycle) => {
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutos</td>
+                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    <Status statusColor="green">Concluído</Status>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </HistoryList>
-
     </HistoryContainer>
-  )
+  );
 }
