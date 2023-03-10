167aee488b16c7f4a6178366e942a6bb5aa43593
leticea <nascimento.leticeah@gmail.com>
2023-03-10 17:07:24 -0300

    criando useEffect para salvar os dados

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, ReactNode, useReducer, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 import {
   addNewCycleAction,
   interruptCurrentCycleAction,
@@ -37,6 +43,12 @@ export function CyclesContextProvider({
   });
 
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState);
+
+    localStorage.setItem("@ignite-timer:cycles-state-1.0.0", stateJSON);
+  }, [cyclesState]);
 
   const { cycles, activeCycleId } = cyclesState;
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
